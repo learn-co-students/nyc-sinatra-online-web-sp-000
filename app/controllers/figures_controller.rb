be6b3717e37b196dde3b39f119e67c80b7b4bc55
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-22 05:17:21 +0000

    Build out Edit form

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,8 +1,53 @@
 class FiguresController < ApplicationController
 
+
+
   get '/figures/new' do
     @titles = Title.all
-    erb :'/landmarks/new'
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    @titles = @figure.titles
+    erb :'/figures/edit'
+  end
+
+  post '/figures/new' do
+
+    figure = Figure.create(name: params[:figure][:name])
+    if params[:figure][:title_ids]
+        params[:figure][:title_ids].each do |id|
+        title = Title.find_by_id(id)
+        figure.titles << title
+      end
+    end
+    if params[:title][:name]
+      title = Title.create(name: params[:title][:name])
+      figure.titles << title
+    end
+    if params[:figure][:landmark_ids]
+      params[:figure][:landmark_ids].each do |id|
+        landmark = Landmark.find_by_id(id)
+        figure.landmarks << landmark
+      end
+    end
+    if params[:landmark][:name]
+      landmark = Landmark.create(name: params[:landmark][:name])
+      figure.landmarks << landmark
+    end
+    figure.save
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
   end
 
 end
