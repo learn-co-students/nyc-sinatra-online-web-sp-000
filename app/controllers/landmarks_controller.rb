fa537cf7a077d579d04d9b22d1afa12838f5f282
Douglas Uretsky <douglasuretsky@gmail.com>
2018-12-15 21:21:31 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -8,6 +8,11 @@ class LandmarksController < ApplicationController
     erb :'landmarks/new'
   end
   
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/show'
+  end
+  
   post '/landmarks' do
     @landmark = Landmark.create(params[:landmark])
     redirect :'/landmarks'
