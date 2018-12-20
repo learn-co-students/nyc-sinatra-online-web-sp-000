2a9106856546747ca5cfaa1e65cae63d3be80b01
aburk3 <aburk3@gmail.com>
2018-12-20 13:40:56 -0600

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,12 +9,22 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-
     @figure = Figure.create(params['figure'])
-    # binding.pry
 
+    unless params['landmark']['name'] == null
+      @landmark = Landmark.create(params['landmark']['name'])
+      @figure.landmarks << @landmark
+    end
+    
+    unless params['title']['name'] == null
+      @title = Title.create(params['title']['name'])
+
+      @figure.titles << @title
+    end
+    # binding.pry
     @figure.save
     redirect to "/figures/#{@figure.id}"
+
   end
 
 end
