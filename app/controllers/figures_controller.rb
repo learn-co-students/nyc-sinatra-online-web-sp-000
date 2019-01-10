509d0adab8f14783729bd74dae90ffab4cb0cc74
Cheng Vue <cvu3@ucsc.edu>
2019-01-10 14:01:29 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,52 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get "/figures" do
+    @figures = Figure.all
+    erb :"figures/index"
+  end
+
+  get "/figures/new" do
+    erb :"figures/new"
+  end
+
+  post "/figures" do
+    @figure = Figure.create(params[:figure])
+    if !params[:landmark].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+    @figure.save
+
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    erb :"figures/show"
+  end
+
+  get "/figures/:id/edit" do
+    @figure = Figure.find(params[:id])
+    erb :"figures/edit"
+  end
+
+  patch "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+    @figure.save
+
+    redirect "/figures/#{@figure.id}"
+  end
 end
