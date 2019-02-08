42b5634f88e7a951493698c28828386940412ad4
github username <github email address>
2019-02-08 14:17:09 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,7 +6,6 @@ class FiguresController < ApplicationController
   end
 
   post "/figures" do
-    puts params
     @figure = Figure.create(name: params[:figure_name])
 
     if !params[:title][:name].nil?
@@ -37,6 +36,45 @@ class FiguresController < ApplicationController
   get "/figures/:figure_id" do
     @figure = Figure.find(params[:figure_id])
     erb :'figures/show'
+  end
+
+  get "/figures" do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get "/figures/:figure_id/edit" do
+    @figure = Figure.find(params[:figure_id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:figure_id' do
+    @figure = Figure.find(params[:figure_id])
+    @figure.update(name: params[:figure_name])
+    if !params[:title][:name].nil?
+      title = Title.create(params[:title])
+      @figure.titles << title
+    end
+
+    if !params[:landmark][:name].blank?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+
+    if !params[:figure][:landmark_ids].nil?
+      @landmark = Landmark.find(params[:figure][:landmark_ids][0])
+      @figure.landmarks << @landmark
+    end
+
+    if !params[:figure][:title_ids].nil?
+      params[:figure][:title_ids].each do |id|
+        title = Title.find(id)
+        @figure.titles << title
+      end
+    end
+
+    redirect to "figures/#{@figure.id}"
+
   end
 
 end
