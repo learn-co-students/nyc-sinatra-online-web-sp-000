e4bf818f9ad47946b7d6fb8899930887369e2166
Robert Tate <btate712@gmail.com>
2019-04-27 14:40:17 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,82 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'figures/edit'
+  end
+
+  post '/figures' do
+    @figure = Figure.new(params[:figure])
+    @figure.save
+
+    if params[:title][:name] != ""
+      title = Title.new(params[:title])
+      @figure.titles << title
+    end
+
+    if params[:landmark][:name] != ""
+      landmark = Landmark.new(params[:landmark])
+      @figure.landmarks << landmark
+    end
+
+    redirect :"figure/#{@figure.id}"
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find(params[:id])
+    figure.name = params[:figure][:name]
+    figure.save
+
+    figure.titles = []
+    if params[:title][:name] != ""
+      title = Title.create(params[:title][:name])
+      figure.titles << title
+    end
+
+    if params[:figure][:title_ids]
+      params[figure][:title_ids].each do |title_id|
+        figure.titles << Title.find(title_id)
+      end
+    end
+
+    figure.landmarks = []
+    if params[:landmark][:name] != ""
+      landmark = Landmark.create(params[:landmark][:name])
+      figure.landmarks << landmark
+    end
+
+    if params[:figure][:landmark_ids]
+      params[figure][:landmark_ids].each do |landmark_id|
+        figure.landmarks << Landmark.find(landmark_id)
+      end
+    end
+
+  end
 end
+
+# params
+# => {"figure"=>{"name"=>"Doctor Who", "title_ids"=>["48"]},
+#  "title"=>{"name"=>""},
+#  "landmark"=>{"name"=>"", "year_completed"=>""},
+#  "submit"=>"Create New Figure"}
