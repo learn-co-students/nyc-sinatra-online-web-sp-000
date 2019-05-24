b342a276f6c3035df065b152ae4c4a7b595217c7
John Hayes <johnnyhayes7@gmail.com>
2019-05-24 20:22:35 +0000

    building Figures Controller

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -13,6 +13,18 @@ class FiguresController < ApplicationController
 
   post '/figures' do
 
-  
+    @landmark = params[:landmark]
+    @landmark_ids = params[:figure][:landmark_ids]
+    @figure = Figure.create(:name => params[:figure][:name])
+    params
+    if !params[:title][:name].empty?
+      title = Title.create(:name => params[:title][:name])
+      @figure.titles << title
+    end
+    # binding.pry
+    if params[:figure][:title_ids]
+
+      title = Title.find
+    end
   end
 end
