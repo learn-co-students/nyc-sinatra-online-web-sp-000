8f28ee63d6a31b016fcc90841dbe4fd1088f9cea
John Hayes <johnnyhayes7@gmail.com>
2019-05-25 17:23:34 +0000

    finished and fixed landmarks

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,23 +14,34 @@ class FiguresController < ApplicationController
   post '/figures' do
 
     @landmark = params[:landmark]
-    
+
     @figure = Figure.create(:name => params[:figure][:name])
 
     if !params[:title][:name].empty?
       title = Title.create(:name => params[:title][:name])
       @figure.titles << title
     end
-    # binding.pry
+
     if params[:figure][:title_ids]
        params[:figure][:title_ids].each do |t|
-        #  binding.pry
+
        title = Title.find_by_id(t)
        @figure.titles << title
        end
     end
 
     if !params[:landmark][:name].empty?
+      landmark = Landmark.create(:name => params[:landmark][:name])
+      @figure.landmarks << landmark
+    end
 
+    if params[:figure][:landmark_ids]
+      params[:figure][:landmark_ids].each do |l|
+        # binding.pry
+        landmark = Landmark.find_by_id(l)
+        @figure.landmarks << landmark
+      end
+    end
+    
   end
 end
