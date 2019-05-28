3d4bcb964ca62f91e9a2ca904ab91d4df05643f8
John Hayes <johnnyhayes7@gmail.com>
2019-05-28 17:02:01 +0000

    landmarks views and controllers

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,18 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+
+  erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(:name => params[:landmarks])
+
+    
+  end
 end
