de6d72b1ef524a14cfb31f5f2eae3f1ff2887e35
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-30 16:37:18 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,6 +5,26 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
+  get '/figures' do
+    @figures = Figure.all
+
+    erb :'figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    # binding.pry
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'figures/edit'
+  end
+
   post '/figures' do
     # binding.pry
 
@@ -16,5 +36,25 @@ class FiguresController < ApplicationController
     if !params[:landmark][:name].empty?
       @figure.landmarks << Landmark.create(params[:landmark])
     end
+
+    @figure.save
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    # binding.pry
+    @figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      # binding.pry
+      @figure.titles << Title.create(params[:title])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    @figure.save
+# binding.pry
+    redirect "/figures/#{params[:id]}"
   end
 end
