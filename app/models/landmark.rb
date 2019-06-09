3453e044aeac409d9c4481fcb07ae1425540952a
Barbara Johnson <johnson.barbara@gmail.com>
2019-06-09 16:05:20 -0500

    Done.

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,3 +1,6 @@
 class Landmark < ActiveRecord::Base
   # add relationships here
+  # A Landmark belongs_to a figure and a figure has_many landmarks.
+  belongs_to :figure
+
 end
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,3 +1,9 @@
 class Title < ActiveRecord::Base
   # add relationships here
+  # A title has_many figure_titles and many figures through figure_titles.
+  has_many :figure_titles
+  has_many :figures, through: :figure_titles
+
+
+
 end
