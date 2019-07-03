8e846dab28adde04b75eb952ff7266d4525328e9
Bry Cox <carl.cc@gmail.com>
2019-07-03 18:42:36 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,49 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures/new' do
+
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/new"
+
+  end
+
+  get '/figures' do
+
+    @figures = Figure.all
+    erb :"/figures/index"
+
+  end
+
+  post '/figures' do
+
+    @title = params[:title]
+    @title_ids = params[:figure][:title_ids]
+    @landmark = params[:landmark]
+    @landmark_ids = params[:figure][:landmark_ids]
+
+    @figure = Figure.create(:name => params[:figure][:name])
+    if !@title[:name].empty?
+      t = Title.create(:name => @title[:name])
+      @figure.titles.push(t)
+    end
+    if @title_ids
+      @title_ids.each do |id|
+        t = Title.find(id)
+        @figure.titles.push(t)
+      end
+    end
+    if !@landmark[:name].empty?
+      l = Landmark.create(:name => @landmark[:name])
+      @figure.landmarks.push(l)
+    end
+    if @landmark_ids
+      @landmark_ids.each do |id|
+        l = Landmark.find(id)
+        @figure.landmarks.push(l)
+      end
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190703170850) do
+ActiveRecord::Schema.define(version: 20190703183159) do
+
+  create_table "figure_titles", force: :cascade do |t|
+    t.integer "title_id"
+    t.integer "figure_id"
+  end
 
   create_table "figures", force: :cascade do |t|
     t.string "name"
