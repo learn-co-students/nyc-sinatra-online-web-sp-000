930a3eeaf9dbf5ce1d1d0c72e0c61dd419fbb32a
Alan Krajina <alankrajina88@gmail.com>
2019-07-23 14:29:39 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,10 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
 get '/figures/new' do
   @landmarks = Landmark.all
   @titles = Title.all
@@ -8,10 +13,10 @@ end
 
 post '/figures' do
   @figure = Figure.new(name: params[:figure][:name])
+#  binding.pry
 
   if !params["title"]["name"].empty?
   @figure.title << Title.find_or_create_by(name: params["title"]["name"])
-
   end
 #  @figure.title = Title.find_or_create_by(name: params["title"]["name"])
 #  @figure.landmark = Landmark.find_or_create_by(name: params["landmark"]["name"])
