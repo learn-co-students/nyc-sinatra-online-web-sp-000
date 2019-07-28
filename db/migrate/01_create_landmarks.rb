5f0bf58befb3b1415ed46653750ba893d4d599d0
Ramon Chavez <rachavez1974@gmail.com>
2019-07-28 17:20:22 -0400

    Create migrations for figures, landmarks, titles, and figure_titles

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,13 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  def up
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :figure_id
+      t.string :year_completed
+    end
+  end
+ 
+  def down
+    drop_table :landmarks
+  end
 end
