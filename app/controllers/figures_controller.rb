e1318c79ec79bf4465a6530ef3a898530f8b32ee
Jonathan Wong <jonathan.wong110@gmail.com>
2019-08-01 22:19:14 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class FiguresController < ApplicationController
   # add controller methods
   
@@ -8,6 +9,13 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     @figure = Figure.create(params[:figure])
+    if !params[:title].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    if !params[:landmark].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
     redirect "/figures/#{@figure.id}"
   end
 
