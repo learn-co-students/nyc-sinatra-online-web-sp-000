b81a249d1540ac2d3c080403d1827e7e8cc5e12e
Abagail Hudson <abagailanne@gmail.com>
2019-08-10 17:32:01 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,49 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.new(params[:figure])
+    if params[:title][:name].present?
+      @figure.titles << Title.create(params[:title])
+    end
+    if params[:landmark][:name].present?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    if params[:title][:name].present?
+      @figure.titles << Title.create(params[:title])
+    end
+    if params[:landmark][:name].present?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+ end
 end
