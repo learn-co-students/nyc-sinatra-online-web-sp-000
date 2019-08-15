8f226855f48afacef171a80d07fd571b7d48c1e2
Malcolm Leake <echelon1089@gmail.com>
2019-08-15 01:01:42 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,36 @@
 class FiguresController < ApplicationController
   # add controller methods
+  
+   get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index' 
+  end
+
+  get '/figures/new' do 
+    @figures = Figure.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do 
+    
+  end
+
+  get '/figures/:id/edit' do 
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  get '/owners/:id' do 
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  patch '/figures/:id' do 
+   
+  end
+  
+  
+  
+  
+  
 end
