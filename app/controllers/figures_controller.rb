0328fe86a7dd5fbe4af01e9efea4baba830131fa
Malcolm Leake <echelon1089@gmail.com>
2019-08-17 00:56:05 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,13 +8,39 @@ class FiguresController < ApplicationController
 
   get '/figures/new' do 
     @titles = Title.all
-    @figures = Figure.all
     @landmarks = Landmark.all
     erb :'/figures/new'
   end
 
   post '/figures' do 
+    @title = params[:title]
+    @title_ids = params[:figure][:title_id]
+    @landmark = params[:landmark]
+    @landmark_ids = params[:landmark_id]
     
+    @figure = Figure.create(:name => params[:figure][:name])
+      if !@title[:name].empty?
+        t=Title.create(:name => @title[:name])
+        @figure.titles << t
+      end
+      if @title_ids
+        @title_ids.each do |id|
+          t=Title.find(id)
+          @figure.titles << t
+      end
+    end 
+    if !@landmark[:name].empty?
+      l = Landmark.create(:name => @landmark[:name])
+      @figure.landmarks << l
+    end
+    if @landmark_ids
+      @landmark_ids.each do |id|
+        l=Landmark.find(id)
+        @figure.landmarks << l
+      end
+    end
+    @figure.save
+    redirect "/figures/#{@figure.id}"
   end
 
   get '/figures/:id/edit' do 
