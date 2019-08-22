a0666cf42551310f3a93b1c2643a52261dab07e4
Malcolm Leake <echelon1089@gmail.com>
2019-08-22 21:56:24 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,17 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  
+  
+  get '/landmarks/new' do
+    @landmarks= Landmark.all
+    erb :'/landmarks/new'
+  end
+  
+  post '/landmarks' do
+    
+  end
+  
+  
+  
+  
 end
