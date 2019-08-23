b8fbb86f28dc204a4bdd79591c594f541b6eff11
Yingqi Chen <chanwk18@hotmail.com>
2019-08-23 22:57:34 +0000

    Done.

--- app/models/figure_title.rb
+++ app/models/figure_title.rb
@@ -1,5 +1,4 @@
-class FigureTitle < ActiveRecord::Base
-  # add relationships here
-  belongs_to :figures
-  belongs_to :titles
+class FigureTitle <ActiveRecord::Base
+  belongs_to :title
+  belongs_to :figure
 end
