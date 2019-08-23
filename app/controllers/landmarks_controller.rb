3c1567cda0d327c5b01eaeb861e9266d51716756
Malcolm Leake <echelon1089@gmail.com>
2019-08-23 22:24:12 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,20 +1,18 @@
 class LandmarksController < ApplicationController
   # add controller methods
-  get '/landmarks'
-  Landmarks.all 
+  get '/landmarks' do
+  @landmarks=Landmark.all 
   erb :'/landmarks/index'
   end
   
   
   get '/landmarks/new' do
-    #binding.pry
     erb :'/landmarks/new'
   end
   
   post '/landmarks' do
     @landmark = Landmark.create(params[:landmark])
     redirect "landmark/#{@landmark.id}"
-    
   end
   
   
