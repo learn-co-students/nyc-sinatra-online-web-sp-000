26755e5b2e35ccec1fca9e5ae6715883b5835b08
Peter Grant <jgrantps@hotmail.com>
2019-09-07 01:57:58 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,18 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+
+  erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+# binding.pry
+@titles = Title.all
+@landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+raise params.inspect
+    # binding.pry
+  end
 end
