a6b2e98298a41ad22ccf69829c7d51981dd45366
Peter Grant <jgrantps@hotmail.com>
2019-09-07 03:15:48 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -21,9 +21,22 @@ class FiguresController < ApplicationController
     erb :'/figures/edit'
   end
 
-  # patch '/figures/:id'
-  #
-  # end
+  patch '/figures/:id' do
+       @figure = Figure.find_by_id(params[:id])
+       @figure.update(params[:figure])
+
+    unless params[:title][:name].empty?
+       @figure.titles << Title.create(params[:title])
+    end
+
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
 
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
