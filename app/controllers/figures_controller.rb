d471d4d79dc01b5d55f17bd5e828f6214c51717a
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-09-20 02:49:21 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,59 @@
+require 'pry'
 class FiguresController < ApplicationController
-  # add controller methods
+
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :"/figures/index"
+  end
+
+# @genres = Genre.all
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    @titles = Title.all
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @title = params[:title]
+    @title_ids = params[:figure][:title_ids]
+    @landmark = params[:landmark]
+    @landmark_ids = params[:figure][:landmark_ids]
+
+    @figure = Figure.create(:name => params[:figure][:name])
+      if !@title[:name].empty?
+         t = Title.create(:name => @title[:name])
+         @figure.titles << t
+      end
+
+      if @title_ids
+        @title_ids.each do |title|
+        t = Title.find(title)
+        @figure.titles << t
+       end
+     end
+
+      if !@landmark[:name].empty?
+        l = Landmark.create(:name => @landmark[:name])
+        @figure.landmarks << l
+      end
+
+      if @landmark_ids
+        @landmark_ids.each do |lm|
+        l = Landmark.find(lm)
+        @figure.landmark << l
+        end
+      end
+
+      @figure.save
+      redirect to "/figures/#{@figure.id}"
+  end
+
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :"/figures/show"
+  end
+
 end
