fd309ebd68664772faf797e70642c687a99032df
Alice Brunel <alc.brunel@gmail.com>
2019-10-03 11:52:24 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,21 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+   get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+  
+  get '/figures/new' do  
+    erb :'/figures/new'
+  end
+  
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+  
+  post '/figures' do 
+    @figure = Figure.create(params[:figure])
+    @figure.titles = params[:title_ids]
+  end
 end
