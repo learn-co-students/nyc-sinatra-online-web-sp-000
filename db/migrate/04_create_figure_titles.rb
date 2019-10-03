04520726d6f9b2f870dc19cf27ecad2fba733e3c
Alice Brunel <alc.brunel@gmail.com>
2019-10-03 11:39:47 +0000

    Done.

--- db/migrate/04_create_figure_titles.rb
+++ db/migrate/04_create_figure_titles.rb
@@ -1,4 +1,4 @@
-class CreateTitleFigures < ActiveRecord::Migration
+class CreateFigureTitles < ActiveRecord::Migration
   def change
     create_table :figure_titles do |t|
       t.integer :title_id
