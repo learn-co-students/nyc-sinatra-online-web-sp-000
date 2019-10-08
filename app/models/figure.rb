2b36900ab5455e4b6cbcec59da12cc808a7c6feb
Frederic Silber <freddy.silber@cloudcoach.com>
2019-10-08 01:29:20 +0000

    Done.

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,3 +1,10 @@
 class Figure < ActiveRecord::Base
   # add relationships here
+  has_many :figure_titles
+  has_many :titles, through: :figure_titles
+  has_many :landmarks
 end
+# class Figure < ActiveRecord::Base
+#   has_many :figure_titles
+#   has_many :titles, through: :figure_titles
+# end
