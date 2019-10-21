fc4503934d3627eb4c1f8f200151d9aabe258be2
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-21 16:28:03 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,15 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :index
+  end
+
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+
+
+
 end
