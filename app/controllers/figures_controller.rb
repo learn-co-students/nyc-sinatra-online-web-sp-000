f617ae3cb6e702190e79f813956a9bd7db43a2aa
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-21 20:30:58 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,28 @@
 class FiguresController < ApplicationController
-  
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new'do
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params)
+
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+
+    erb '/figures/***/edit'
+  end
+
+
 end
