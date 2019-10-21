2216f5caa0646317df3ec2fe80c23e7c0522ee77
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-21 17:11:05 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,13 +2,26 @@ class LandmarksController < ApplicationController
 
   get '/landmarks' do
     @landmarks = Landmark.all
-    erb :index
+    erb :'landmarks/index'
   end
 
   get '/landmarks/new' do
     erb :"landmarks/new"
   end
 
+  get 'landmark/:id' do
+    @landmark = Landmark.find_by(params[:id])
+    erb :show
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(name: params["name"], year_completed: params["landmark[year_completed]"])
+    @landmark.figure = Figure.find_or_create_by(name: params["figure name"])
+    @landmark.figure.title = FigureTitle.find_or_create_by(title: params["figure title"])
+    @landmark.save
+
+    redirect '/lanmarks/#{@landmark.id}'
+  end
 
 
 
