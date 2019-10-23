32185d9e6ddc352ad78a6b06df7e13c417b51db4
root <root@TheONEJesusLove.localdomain>
2019-10-23 12:34:36 -0700

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,15 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get "/" do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get "/new" do
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    erb :'landmarks/new'
+
+  end
+
 end
