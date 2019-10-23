e9bd69df7c981eafd807f67883da583349b37381
Lauren McCoy <lm_lutec@outlook.com>
2019-10-23 14:01:48 -0400

    working on figure controller

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -3,7 +3,8 @@ require 'pry'
 class FiguresController < ApplicationController
   
   get '/figures/new' do 
-    
+    @landmarks = Landmark.all 
+    @titles = Title.all 
     erb :'figures/new' 
   end 
 end
