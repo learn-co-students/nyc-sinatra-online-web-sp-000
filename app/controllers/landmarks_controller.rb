87ffbe89faeaa7bd1035488d8e4a634c64edbdfa
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-10-23 14:58:41 -0700

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,15 +1,14 @@
 class LandmarksController < ApplicationController
   # add controller methods
-  get "/" do
+  get "/landmarks" do
     @landmarks = Landmark.all
     erb :'landmarks/index'
   end
 
-  get "/new" do
+  get "/landmarks/new" do
     @landmarks = Landmark.all
     @figures = Figure.all
     erb :'landmarks/new'
-
   end
 
 end
