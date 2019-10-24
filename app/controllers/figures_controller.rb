2143e32053ff1d519a2da162338d871a25ec0a95
Lauren McCoy <lm_lutec@outlook.com>
2019-10-24 08:43:15 -0400

    completed figures/index erb

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,6 +5,12 @@ class FiguresController < ApplicationController
   get '/figures/new' do 
     @landmarks = Landmark.all 
     @titles = Title.all 
-    erb :'figures/new' 
+    erb:'/figures/new' 
+  end
+  
+  
+  get '/figures' do 
+    @figures= Figure.all 
+    erb:'/figures/index'
   end 
 end
