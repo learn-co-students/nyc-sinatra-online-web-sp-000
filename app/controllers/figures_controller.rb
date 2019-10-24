27a10678223ebab3b29367472049a275ae312066
Lauren McCoy <lm_lutec@outlook.com>
2019-10-24 13:15:34 -0400

    fixed bug in landmarks creation

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,9 +8,38 @@ class FiguresController < ApplicationController
     erb:'/figures/new' 
   end
   
-  
   get '/figures' do 
     @figures= Figure.all 
     erb:'/figures/index'
   end 
+  
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+  
+    if params[:title][:name] != ""
+      @title = Title.create(params[:title]) 
+      @figure.titles << @title 
+    end 
+
+    if params[:landmark][:name] != ""
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark 
+    end 
+      @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end 
+  
+
+  get '/figures/:id' do 
+    @figure = Figure.find_by_id(params[:id])
+    erb:'/figures/show'
+  end 
+
+  get '/figures/:id/edit' do 
+    @landmarks = Landmark.all 
+    @titles = Title.all 
+    @figure = Figure.find_by_id(params[:id])
+    erb:'/figures/edit'
+  end 
+
 end
