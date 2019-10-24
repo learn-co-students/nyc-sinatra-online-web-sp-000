7cf756c73e909d67fdc667b255a7b36c25ae527b
Mark Daniel  Delgado <markpnoi26@gmail.com>
2019-10-24 22:36:59 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,61 @@
 class FiguresController < ApplicationController
   # add controller methods
+  
+  get "/figures" do
+    @figures = Figure.all
+    erb :"/figures/index"
+  end
+  
+  get "/figures/new" do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/new"
+  end
+  
+  post "/figures/new" do
+    @new_figure = Figure.create(name: params[:figure][:name],  landmark_ids: params[:figure][:landmark_ids], title_ids: params[:figure][:title_ids])
+    @new_title = Title.create(name: params[:title][:name]) unless params[:title][:name] == ""
+    @new_landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed]) unless params[:landmark][:name] == ""
+    
+    # if !(params[:figure][:landmark_ids].empty?)
+    #   params[:figure][:landmark_ids].each {|landmark_id| @new_figure.landmarks << Landmark.find_by_id(landmark_id)}
+    # end
+    
+    # if !(params[:figure][:title_ids].empty?)
+    #   params[:figure][:title_ids].each {|title_id| @new_figure.titles << Title.find_by_id(title_id)}
+    # end
+    
+    # any reason why above code doesn't work? or only works for one then skips the rest?
+    
+    @new_figure.titles << @new_title if @new_title
+    @new_figure.landmarks << @new_landmark if @new_landmark
+    @new_figure.save
+    redirect "/figures/#{@new_figure.id}"
+  end
+  
+  get "/figures/:id" do
+    @figure = Figure.find_by_id(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/show"
+  end
+  
+  get "/figures/:id/edit" do
+    @figure = Figure.find_by_id(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"figures/edit"
+  end
+  
+  patch "/figures/:id" do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(name: params[:figure][:name], landmark_ids: params[:figure][:landmark_ids], title_ids: params[:figure][:title_ids])
+    @new_title = Title.create(name: params[:title][:name]) unless params[:title][:name] == ""
+    @new_landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed]) unless params[:landmark][:name] == ""
+    @figure.titles << @new_title if @new_title
+    @figure.landmarks << @new_landmark if @new_landmark
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+  
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,5 +1,5 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
   get "/landmarks" do
     @landmarks = Landmark.all
     erb :'landmarks/index'
@@ -9,6 +9,29 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     @figures = Figure.all
     erb :'landmarks/new'
+  end
+  
+  post "/landmarks/new" do
+    @new_landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks/#{@new_landmark.id}"
+  end
+  
+  get "/landmarks/:id" do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"landmarks/show"
+  end
+  
+  get "/landmarks/:id/edit" do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"landmarks/edit"
+  end
+  
+  patch "/landmarks/:id" do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.name = params[:landmark][:name]
+    @landmark.year_completed = params[:landmark][:year_completed]
+    @landmark.save
+    redirect "/landmarks/#{params[:id]}"
   end
 
 end
