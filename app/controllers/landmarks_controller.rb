dcc319bcb645813830222f979f8542b3a311bac4
Lauren McCoy <lm_lutec@outlook.com>
2019-10-26 11:05:12 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -25,7 +25,19 @@ class LandmarksController < ApplicationController
     erb:'/landmarks/edit'
   end 
   
+  patch '/landmarks/:id' do 
+    @landmark = Landmark.find_by_id(params[:id]) 
+      if params[:landmark][:name] != ""
+        @landmark.name = params[:landmark][:name]
+      end 
 
+      if params[:landmark][:year_completed] != ""
+        @landmark.year_completed = params[:landmark][:year_completed]
+      end 
+      @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end 
 
 
 end
+
