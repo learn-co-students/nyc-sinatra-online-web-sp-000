7a43cc48f30f4a289a32843a28af25bb94f49f6c
BlythePollard <github email address>
2020-01-04 13:09:28 -0700

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,26 +1,43 @@
 class LandmarksController < ApplicationController
   get '/landmarks/new' do 
+    erb :'landmarks/new'
     #view form to create new landmark
   end
   
-  post '/landmarks/new' do 
+  post '/landmarks' do 
+    @landmark = Landmark.create(params[:landmark])
+    unless params[:figure][:name].empty?
+      @landmark.figures << Figure.create(params[:figure])
+    end
+    @landmark.save
+    redirect to "/landmarks/#{landmark.id}"
     #post- create new landkarm
   end
   
   get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
     #list all landmarks (index)
   end 
 
   get '/landmarks/:id' do 
-    #see single landmark 
+    @landmark = Landmark.find_by(params[:id])
+    erb :'landmarks/show'
   end
   
   get '/landmarks/:id/edit' do
-    #view form to edit a landmark
+    @landmark = Landmark.find_by(params[:id])
+    erb :'landmarks/edit'    
   end
 
   patch '/landmarks/:id' do 
-    #patch- edit a single landmark
-    #redirect to '/landmarks/:id'
+  @landmark = Landmark.find_by(params[:id])
+  @landmark.update(params[:landmark])
+  unless params[:figure][:name].empty?
+    @landmark.figures << Figure.create(params[:figure])
   end
+  @landmark.save  
+  redirect("/figures/#{@landmark.id}")
+end
+  
 end
