fd2f98931c18a3158c458d2c757f4c0dfc45bc55
RickLearnsToCode <rick.rosetti@gmail.com>
2020-01-22 07:53:10 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,59 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    # binding.pry
+    @figure = Figure.new(params[:figure][:name])
+
+    if !params[:figure][:title_ids].empty?
+      @figure_titles = []
+      params[:figure][:title_ids].each do |id|
+  
+        @figure_titles << Title.select {|title| title.id == id}
+      end
+  
+      @figure.titles = @figure_titles
+    end
+    
+
+
+    @figure.landmarks = params[:figure][:landmarks]
+    @figure.save
+
+    redirect("/figures/#{@figure.id}")
+  end
+
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/index'
+  end
+
+
+
+
+
+
 end
