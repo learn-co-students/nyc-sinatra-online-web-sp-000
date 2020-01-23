b22379dac45d8a15868024d536b4b7055fbcc4d6
github username <github email address>
2020-01-22 18:07:47 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,26 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    figure_name = params[:figure][:name]
+    @figure = Figure.create(name: figure_name)
+    if !params[:figure][:title_ids].empty?
+      title_ids = params[:figure][:title_ids].join
+      #binding.pry
+
+      @figure.title_ids << title_ids
+      @figure.save
+    end
+    #binding.pry
+    if !params[:title][:name].empty?
+      new_title = params[:title][:name]
+      @title = Title.create(name: new_title)
+      @figure.title_ids << @title.id
+      @figure.save
+    end
+
+  end
+
 end
