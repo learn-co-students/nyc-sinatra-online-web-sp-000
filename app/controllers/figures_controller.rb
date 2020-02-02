713231428d4b1dac746865e0a755524fcee7ba5f
Lukie Kang <Lukie Kang>
2020-02-02 22:45:29 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,44 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get "/figures" do
+    @figures = Figure.all
+    erb:"figures/index"
+  end
+
+  get "/figures/new" do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb:"figures/new"
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    erb:"figures/show"
+  end
+
+  post "/figures" do
+
+    @figure = Figure.create(params[:figure])
+
+    @title_name = params[:title][:name]
+    @landmark_name = params[:landmark][:name]
+
+    if !Title.find_by(name: @title_name)
+      @title = Title.create(name: @title_name)
+      @figure.titles << @title
+    end
+
+    if !Landmark.find_by(name: @t_name)
+      @landmark = Landmark.create(name: @landmark_name)
+      @figure.landmarks << @landmark
+    end
+  
+  end
+
+  patch "/figures/:id/edit" do
+    "Figures Update Route"
+  end
+
+  
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,26 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  get "/landmarks" do
+    "landmarks Index Route"
+    erb :index
+  end
+
+  get "/landmarks/new" do
+    "landmarks New Route"
+    erb :new
+  end
+
+  get "/landmarks/:id" do
+    "landmarks Show Route"
+    erb :show
+  end
+
+  post "/landmarks" do
+    "landmarks POST Route"
+  end
+
+  patch "/landmarks/:id/edit" do
+    "landmarks Update Route"
+  end
+  
 end
