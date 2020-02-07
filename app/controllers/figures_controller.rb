3245df545127600c50b167b639c49d4143816679
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-02-07 17:05:32 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,36 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do 
+    @figures = Figure.all
+    erb :'figures/index'
 end
+
+get '/figures/new' do
+  erb :'figures/new'
+end
+
+post '/figures' do
+  
+  @figures = figure.create(name: params[:name])
+  redirect to 'figures/#{@figure.id}'
+end
+
+get '/figures/:id' do
+  @figure = Figure.find_by_id(params[:id])
+  erb :'figures/show'
+end
+
+get '/figures/:id/edit' do
+  @figure = Figure.find_by_id(params[:id])
+  erb 'figures/edit'
+end
+
+post '/figures/:id' do 
+  @figure = Figure.find_by_id(params[:id])
+  @figure = Figure.update(name: params[:name])
+  redirect to '/figures/#{@figure.id}'
+end
+
+end
+
+ 
