7eb69cc5b48fd9fb4652502d03a77be07eddf0ae
Chris Cheong <chrisxcheong@gmail.com>
2020-02-08 06:31:19 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,5 +1,4 @@
 class LandmarksController < ApplicationController
-  # add controller methods
   get '/landmarks' do
     @landmarks = Landmark.all
     @figures = Figure.all
@@ -23,5 +22,13 @@ end
   get '/landmarks/:id/edit' do
     @landmark = Landmark.find(params[:id])
     erb :"landmarks/edit"
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params[:landmark][:name]
+    @landmark.year_completed = params[:landmark][:year_completed]
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
   end
 end
