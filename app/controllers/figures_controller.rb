f34c07c23ddd2c9e489dd01237e7a66e3f026f36
imjoshellis <joshe523@gmail.com>
2020-03-15 09:40:30 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,41 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get "/figures/new" do
+    erb :'figures/new'
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get "/figures/:id/edit" do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  post "/figures" do
+    @figure = Figure.new(params[:figure])
+    if params[:title][:name].size > 0
+      @figure.titles << Title.create(params[:title])
+    end
+    if params[:landmark][:name].size > 0
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+  end
+
+  patch "/figures/:id" do
+    @figure = Figure.find(params[:id]).update(params[:figure])
+    if params[:title][:name].size > 0
+      @figure.titles << Title.create(params[:title])
+    end
+    if params[:landmark][:name].size > 0
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+  end
+
+  get "/figures" do
+    erb :'figures/index'
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,35 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get "/landmarks/new" do
+    erb :'landmarks/new'
+  end
+
+  get "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get "/landmarks/:id/edit" do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  post "/landmarks" do
+    @landmark = Landmark.new(params[:landmark])
+    if params[:figure][:name].size > 0
+      @landmark.figure = Figure.create(params[:figure])
+    end
+    @landmark.save
+  end
+
+  patch "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id]).update(params[:landmark])
+    if params[:figure][:name].size > 0
+      @landmark.figure = Figure.create(params[:figure])
+    end
+    @landmark.save
+  end
+
+  get "/landmarks" do
+    erb :'landmarks/index'
+  end
 end
