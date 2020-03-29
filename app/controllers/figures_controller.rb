da885c5ff92070acc42b26d13f86854d82e817f9
Alex Velasquez <jtinnesco@gmail.com>
2020-03-29 23:13:28 +0000

    created figures/show.erb

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,7 +6,29 @@ class FiguresController < ApplicationController
   end
 
   get "/figures/new" do
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :'figures/new'
   end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(params[:id])
+    erb :'figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params["figure"])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+
 
 end
