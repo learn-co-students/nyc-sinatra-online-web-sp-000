382e377be96e29d0ef3bdec803d0b68a11fa8b04
github username <github email address>
2020-03-31 17:02:44 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,17 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures/new' do 
+    @figures = Figure.all 
+    @landmarks = Landmark.all 
+    @titles = Title.all 
+    
+    erb :'/figures/new'
+  end 
+
+  post '/figures' do 
+    @figure = Figure.new
+
+    redirect to :"/figures/#{@figure.id}"
+  end 
+
 end
