cd056ffeb9911538e45df55ea9952e73e2309744
github username <github email address>
2020-03-31 19:24:22 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,20 +14,17 @@ class FiguresController < ApplicationController
   end 
 
   post '/figures' do 
-    #binding.pry
     @figure = Figure.new(params["figure"])
     if !params["title"]["name"].empty?
       @figure.titles << Title.create(name: params["title"]["name"])
     end 
-    #binding.pry 
+  
     if !params["landmark"]["name"].empty?
       @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
     end 
 
-    #binding.pry
-  
     @figure.save 
-    #binding.pry 
+
     redirect to :"/figures/#{@figure.id}"
   end 
 
