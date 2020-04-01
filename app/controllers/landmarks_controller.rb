a452c93743b9d0149569a93cebfcbaa99f483cee
github username <github email address>
2020-03-31 23:48:43 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,26 +1,26 @@
 class LandmarksController < ApplicationController
   
   get '/landmarks' do 
-    @landmarks = Landmark.all
 
     erb :'/landmarks/index'
   end 
 
   get '/landmarks/new' do 
+    @landmarks = Landmark.all
     
     erb :'/landmarks/new'
   end 
 
   post '/landmarks/' do 
-    if !params["landmark"]["name"].empty? && !params["landmark"]["landmark_year_completed"]
+    
+    if !params["landmark"]["name"].empty?
       @landmark.name = Landmark.create(name: params["landmark"]["name"])
     end 
 
     if !params["landmark"]["landmark_year_completed"]
-      @landmark.year_completed = Landmark.create(name: params["landmark"]["year_completed"])
+      @landmark.year_completed = Landmark.create(year_completed: params["landmark"]["landmark_year_completed"])
     end 
-
-    @figure.save 
+    @landmark.save 
 
     redirect to :"/landmarks/#{@landmark.id}"
   end 
