e362019265e38d35db7fbb4fa81b43a0a2d50e73
Winston Pemberton <winstonpemberton@gmail.com>
2020-04-08 23:50:01 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,21 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.find_or_create_by(params[:figure])
+
+    if !params[title][name].empty?
+      @figure.titles << Title.find_or_create_by(params[:title])
+    end
+
+    if !params[landmark][name].empty?
+      @figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+    end
+  end
 end
