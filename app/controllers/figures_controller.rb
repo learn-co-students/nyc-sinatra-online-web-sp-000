5f43ae26c931285fbb3f0176cdcb2d21680ab4b3
Winston Pemberton <winstonpemberton@gmail.com>
2020-04-09 22:51:52 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,22 +8,61 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    binding.pry
-    @figure = Figure.find_or_create_by(params[:figure])
+    @figure = Figure.create(params[:figure])
 
-    if !params[title][name].empty?
-      @figure.titles << Title.find_or_create_by(params[:title])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
     end
 
-    if !params[landmark][name].empty?
-      @figure.landmarks << Landmark.find_or_create_by(params[:landmark])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
     end
     redirect to "/figures/#{@figure.id}"
   end
 
-  get '/figures/id' do
+  get '/figures/:id' do
     @figure = Figure.find(params[:id])
-    erb :"figures/show"
+    erb :'figures/show'
   end
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    binding.pry
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    redirect to "/figures/#{@figure.id}"
+  end
 end
+# patch '/owners/:id' do
+#     ####### bug fix
+#     if !params[:owner].keys.include?("pet_ids")
+#     params[:owner]["pet_ids"] = []
+#     end
+#     #######
+#
+#     @owner = Owner.find(params[:id])
+#     @owner.update(params["owner"])
+#     if !params["pet"]["name"].empty?
+#       @owner.pets << Pet.create(name: params["pet"]["name"])
+#     end
+#     redirect "owners/#{@owner.id}"
+# end
