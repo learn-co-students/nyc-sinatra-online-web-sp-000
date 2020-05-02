83f9764f2d8f2b4556bac812fffec33f4c71e378
Anthony Tilelli <Anthony@tilelli.me>
2020-05-02 19:45:16 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,10 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+  post '/figures' do
+    @figure = Figure.create!(params['figure'])
+    @figure.landmarks << Landmark.create!(params[:landmark]) unless params[:landmark][:name].empty?
+  end
 end
