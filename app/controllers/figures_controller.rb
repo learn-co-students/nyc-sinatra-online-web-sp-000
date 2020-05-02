e876cf6d58adcf9be4bec4df4105663c8218a265
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-02 00:53:57 -0400

    landmarks working

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -35,4 +35,31 @@ class FiguresController < ApplicationController
     erb :'figures/show'
   end
 
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @all_titles = Title.all
+    @all_landmarks = Landmark.all
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    # Create a new title if necessary and add
+    # to this new figure's titles
+    if !params[:title][:name].empty?
+      title = Title.create(name: params[:title][:name])
+      @figure.titles << title
+    end
+
+    # Create a new landmark if necessary
+    if !params[:landmark][:name].empty?
+      landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << landmark
+    end
+
+    redirect "/figures/#{@figure.id}"
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,39 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  get '/landmarks' do
+    @all_landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    @all_figures = Figure.all
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    Figure.find_or_create_by(name: params[:figure][:name]).landmarks << @landmark
+
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    @all_figures = Figure.all
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    Figure.find_or_create_by(name: params[:figure][:name]).landmarks << @landmark
+
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
 end
