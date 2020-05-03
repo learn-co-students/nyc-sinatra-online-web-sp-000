a90d1f34295fa5b64fcfb5585f94039484902223
Anthony Tilelli <Anthony@tilelli.me>
2020-05-02 23:27:52 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -31,8 +31,8 @@ class FiguresController < ApplicationController
 
   # ----- Patch ----
   patch '/figures/:id' do
-    figure_id
-    @figure.update!(params[:figure])
+    @figure = figure_id
+    @figure.update!({figure: params[:figure], year_completed:params['landmark']['year_completed']})
     add_title_and_landmark
     @figure.save
     redirect to "/figures/#{@figure.id}"
