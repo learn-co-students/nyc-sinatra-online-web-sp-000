21a0eb922f9cd775bf4cf64fc480dc0b9cd38509
Brian Vehige <brianvehige@gmail.com>
2020-05-07 20:38:56 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,17 @@
 class FiguresController < ApplicationController
   # add controller methods
+  
+  get '/figures' do
+    @figures = Figure.all
+    erb :"/figures/index"
+  end
+  
+  get '/figures/new' do 
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :"/figures/new"
+  end
+
+
 end
