5f2df0c564afd255faf1e3e6afe9436cbb4a7d47
ksummerill <kaitlynsummerill@gmail.com>
2020-05-11 19:03:10 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,7 +8,6 @@ class FiguresController < ApplicationController
   get '/figures/new' do
     @landmarks = Landmark.all
     @titles = Title.all
-    # binding.pry
     erb :'figures/new'
   end
 
@@ -17,9 +16,25 @@ class FiguresController < ApplicationController
     erb :'figures/show'
   end
 
-  # post '/figures' do
-  #
-  # end
+  # allows you to create a new figure with a title
+  # allows you to create a new figure with a landmark
+  # allows you to create a new figure with a new title
+  # allows you to create a new figure with a new landmark
+  # params = {"figure"=>{"name"=>"figure params", "title_ids"=>["1"]}, "title"=>{"name"=>""}, "landmark"=>{"name"=>"", "year"=>""}}
+  post '/figures' do
+    # #<Figure:0x00007f8f92d3a0f8 id: 502, name: "Doctor Who">
+    @figure = Figure.create(params[:figure])
+
+    # #<Title:0x00007f8f91d072d0 id: 295, name: "Time Lord">
+    @figure.titles = Title.create(name: params[:title][:name])
+    binding.pry
+    # @figure.titles = @titles
+    @figure.save
+
+    # @figure.landmark = Landmark.find_or_create_by(name: params[:landmark][:name])
+
+
+  end
 
   get '/figures/:id/edit' do
     @figure = Figure.find_by(params[:id])
