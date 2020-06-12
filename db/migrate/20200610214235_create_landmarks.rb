828690c198c8c978db179c570243cdd470f020de
Elliot Blanchard <git@elliotblanchard.com>
2020-06-11 23:55:51 -0400

    Done.

---
+++ db/migrate/20200610214235_create_landmarks.rb
@@ -0,0 +1,10 @@
+class CreateLandmarks < ActiveRecord::Migration
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :figure_id  
+      t.integer :year_completed    
+      t.timestamps null: false
+    end     
+  end
+end
