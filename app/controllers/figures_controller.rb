c729f9fedf99f1846182a75635738f10c7fa9fd9
ting682 <tchung682@gmail.com>
2020-07-13 16:15:49 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,52 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    #binding.pry
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    erb :'figures/show'
+  end
+
+
+
+  post '/figures' do
+    
+    @figure = Figure.find_or_create_by(name: params[:figure][:name])
+    
+
+    binding.pry
+    if params[:landmark][:name] == ""
+      
+      @figure.landmarks.push(Landmark.find(params[:figure][:landmark_ids]))
+      
+    else
+      @figure.landmarks.push(Landmark.find_or_create_by(name: params[:landmark][:name]))
+      
+    end
+    
+    if params[:title][:name] == ""
+      @figure.titles.push(Title.find(params[:figure][:title_ids]))
+     
+    else
+      @figure.titles.push(Title.find_or_create_by(name: params[:title][:name]))
+      
+    end
+    
+
+    
+
+    @figure.save
+    
+    redirect to "/figures/#{@figure.id}"
+  end
 end
--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,26 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  def up
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :year_completed
+      t.integer :figure_id
+    end
+
+    create_table :figures do |t|
+      t.string :name
+    end
+
+    create_table :titles do |t|
+      t.string :name
+    end
+
+
+  end
+
+  def down
+    drop_table :landmarks
+    drop_table :figures
+    drop_table :titles
+    drop_table :figure_titles
+  end
 end
