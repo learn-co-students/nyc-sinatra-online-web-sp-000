65775d30e5c1af83c6125f062806286bb41d3deb
AustinRhoads <austinrrhoads@ymail.com>
2020-07-19 22:28:07 -0500

    all tests pass

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,84 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  post '/figures' do
+      ####### bug fix
+      if !params[:figure].keys.include?("landmark_ids")
+        params[:figure]["landmark_ids"] = []
+      end
+        #######
+           ####### bug fix
+      if !params[:figure].keys.include?("title_ids")
+        params[:figure]["title_ids"] = []
+      end
+        #######
+   
+  
+    if !params[:landmark][:name].empty?
+      @new_landmark = Landmark.create(params[:landmark])
+      params[:figure][:landmark_ids] << @new_landmark.id
+    end
+
+    if !params[:title][:name].empty?
+      @new_title = Title.create(params[:title])
+      params[:figure][:title_ids] << @new_title.id
+    end
+    @figure = Figure.create(params[:figure])
+    redirect to "/figures/#{@figure.id}"
+
+  end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'figures/new'
+  end
+  
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    
+       ####### bug fix
+       if !params[:figure].keys.include?("landmark_ids")
+        params[:figure]["landmark_ids"] = []
+      end
+        #######
+           ####### bug fix
+      if !params[:figure].keys.include?("title_ids")
+        params[:figure]["title_ids"] = []
+      end
+        #######
+   
+  
+    if !params[:landmark][:name].empty?
+      @new_landmark = Landmark.create(params[:landmark])
+      params[:figure][:landmark_ids] << @new_landmark.id
+    end
+
+    if !params[:title][:name].empty?
+      @new_title = Title.create(params[:title])
+      params[:figure][:title_ids] << @new_title.id
+    end
+    
+    @figure = Figure.find(params[:id])
+    
+    @figure.update(params[:figure])
+    redirect "/figures/#{@figure.id}"
+  end
+
 end
