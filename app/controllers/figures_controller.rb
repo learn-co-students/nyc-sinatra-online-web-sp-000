5bf6b6d3114202ebd372240cfe24a44c43dfc9da
The-Gentleman <zraja4402@gmail.com>
2020-07-29 12:07:33 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,48 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  
+  get '/figures' do 
+    @figure = Figure.all
+    erb :'figures/index'
+  end 
+
+  get '/figures/new' do 
+    erb :'figures/new'
+  end 
+
+  get '/figures/:id' do 
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end 
+
+  get '/figures/:id/edit' do 
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end 
+
+  post '/figures' do 
+    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:figure][:title]) 
+    end 
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:figure][:landmark])
+    end 
+
+    @figure.save
+  end 
+
+  patch '/figures/:id' do 
+    @figure = Figure.find_by_id(params[:id])
+
+    if !params.empty?
+      @figure.name = params[:figure][:name]
+    end 
+
+    @figure.save
+  end 
+
+
+
 end
