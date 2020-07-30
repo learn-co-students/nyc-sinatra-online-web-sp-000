fc09061d9576b81a2b49dff32dd2ef18e57ba790
The-Gentleman <zraja4402@gmail.com>
2020-07-30 13:01:04 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -35,7 +35,7 @@ class FiguresController < ApplicationController
 
   patch '/figures/:id' do 
     @figure = Figure.find_by_id(params[:id])
-    binding.pry
+
     @figure.update(params[:figure])
     unless params[:title].empty?
       @figure.titles << Title.create(params[:title])
