c8b386aeeaa4c8c1ad668df53aff9b2a17aad683
ismayton <ian.mayton@gmail.com>
2020-09-05 12:39:40 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -7,20 +7,26 @@ class FiguresController < ApplicationController
   post '/figures' do
     @figure = Figure.create(name: params[:figure][:name])
     
-    params[:figure][:title_ids].each do |id|
-      @figure.titles << Title.find(id)
-    end 
-    if !params[:title][:name].empty?
+    if params[:figure][:title_ids]
+      params[:figure][:title_ids].each do |id|
+        @figure.titles << Title.find(id)
+      end 
+    end
+
+    if params[:title][:name]
       @figure.titles << Title.create(name: params[:title][:name])
     end 
-
-    params[:figure][:landmark_ids].each do |id|
-      @figure.landmarks << Landmark.find(id)
+    if params[:figure][:landmark_ids]
+      params[:figure][:landmark_ids].each do |id|
+        @figure.landmarks << Landmark.find(id)
+      end
+    end 
+  
+    if params[:landmark][:name]
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
     end
-    if !params[:landmark][:name].empty?
-      @figure.landmarks << Landmark.create(name: params[:landmark][:name], year: params[:landmark][:year])
-    end
-
+    @figure.save
+    
   end 
 
   
