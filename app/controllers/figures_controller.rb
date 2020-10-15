e693f995daf0b7d8c08cbf638f41004fe53cf819
sparsell <hello@sparsell.com>
2020-10-15 11:33:30 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,47 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  ### create ###
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    erb :'figures/new'
+  end
+
+
+  post '/figures' do 
+    @figure = Figure.create(name: params[:figure][:name])
+    @figure.landmarks << Landmark.find_or_create_by(name: params[:name])
+    @figure.titles << Title.find_or_create_by(name: params[:name])
+    erb :'figures/show'
+  end
+
+  ### read ###
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  ### update ###
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+  @titles = Title.all
+  @landmarks = Landmark.all
+    redirect "/figures/#{@figure.id}"
+  end
+
+
+  ### delete ###
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,37 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  ### CREATE ###
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do 
+    @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    erb :'/landmarks/show'
+  end
+
+  ###  READ ###
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  ### UPDATE ###
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit' 
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+    erb :"/landmarks/show"
+  end
+
+
 end
