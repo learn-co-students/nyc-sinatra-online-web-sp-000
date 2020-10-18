7973e2af4d6da6b124bac1ebd4246efd206315d5
dsulliv4 <dsulliv4@binghamton.edu>
2020-10-18 17:31:00 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,23 @@
 class FiguresController < ApplicationController
   
   get '/figures/new' do
+    @figures = Figure.all
     erb :'figures/new'
   end 
+
+  post '/figures/' do
+    @figure = Figure.create(params[:figure])
+      unless params[:landmark][:name].empty?
+        @figure.landmarks << Landmark.create(params[:landmark])
+   end 
+
+   unless params[:title][:name].empty?
+    @figure.titles << Title.create(params[:title])
+  end
+
+  @figure.save
+  redirect to "/figures/#{@figure.id}"
+end 
+
 
 end 
