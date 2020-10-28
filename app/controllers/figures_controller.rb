90325de80b4e59ac1a5553b592a0e5d4f7241a50
codyalvarez <jedi.alvarez805@gmail.com>
2020-10-28 09:32:35 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,26 +5,48 @@ class FiguresController < ApplicationController
     erb :'figures/index'
   end
 
-  get '/figures/new' do     
+  get '/figures/new' do
+    @landmarks = Landmark.all     
     erb :'figures/new'
   end
 
-  get '/figures/:id' do
-    @figure = Figure.find(params[:id])
-    erb :'/figures/show'
+  # if there is no assoication between landmark and figure then create one, if there is then find by id
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params["landmark"]["figure"].empty?
+      @figure.landmark = Landmark.create(name: params["landmark"]["name"])
+    else
+      @figure.landmark = Landmark.find_by_id(params[:figure][:landmark_id])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
   end
 
   get '/figures/:id/edit' do
     @figure = Figure.find(params[:id])
-    erb :'/figures/edit'
+    @landmark = Landmark.all
+    erb :'figures/edit'
   end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  # allow the user to edit figure name, and which figure it associatees with 
   patch '/figures/:id' do
     @figure = Figure.find(params[:id])
-    @figure.update(name: params["figure"]["name"])  
+    @figure.update(name: params["figure"]["name"]) 
+    
+    if !params["landmark"]["figure"]
+      @figure.landmarks = Landmark.create(name: params["landmark"]["figure"])
+    else
+      @figure.landmarks = Landmark.find_by_id(params[:figure][:landmark_id])
+    end
+    @figure.save
 
     redirect to "/figures/#{@figure.id}"
   end
-
 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -6,22 +6,45 @@ class LandmarksController < ApplicationController
   end
 
   get '/landmarks/new' do
+    @figures = Figure.all
     erb :'landmarks/new'
   end
 
-  get '/landmarks/:id' do
-    @landmark = Landmark.find(params[:id])
-    erb :'/landmarks/show'
+  # if there is no association between figure and landmark then create on, if there one exist then find by id
+  post '/landmarks' do
+    @landmark = Landmarks.create(params[:landmark])
+    if !params["figure"]["landmark"].empty?
+      @landmark.figure = Landmark.create(name: params["figure"]["name"])
+    else
+      @landmark.figure = Figure.find_by_id(params[:landmark][:figure_id])
+    end
+
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
   end
 
   get '/landmarks/:id/edit' do
     @landmark = Landmark.find(params[:id])
-    erb :'/landmarks/edit'
+    @figure = Figure.all
+    erb :'landmarks/edit'
   end
 
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  # allow the user to edit a landmark name, and which figure it associates with 
   patch '/landmarks/:id' do 
     @landmark = Landmark.find(params[:id])
     @landmark.update(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"])
+
+    if !params["figure"]["landmark"]
+      @landmark.figure = Figure.create(name: params["figure"]["landmark"])
+    else
+      @landmark.figure = Figure.find_by_id(params[:landmark][:figure_id])
+    end
+    @landmark.save 
 
     redirect to "/landmarks/#{@landmark.id}"
   end
