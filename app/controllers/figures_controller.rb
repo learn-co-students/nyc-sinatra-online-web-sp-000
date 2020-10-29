f8a6b3af03ffcae3c83d906be622c5bb6e247fb2
codyalvarez <jedi.alvarez805@gmail.com>
2020-10-28 21:34:59 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,10 +14,12 @@ class FiguresController < ApplicationController
   # if there is no assoication between landmark and figure then create one, if there is then find by id
   post '/figures' do
     @figure = Figure.create(params[:figure])
-    if !params["landmark"]["figure"].empty?
-      @figure.landmark = Landmark.create(name: params["landmark"]["name"])
-    else
-      @figure.landmark = Landmark.find_by_id(params[:figure][:landmark_id])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(name: params[:title][:name])
     end
 
     @figure.save
@@ -27,6 +29,7 @@ class FiguresController < ApplicationController
   get '/figures/:id/edit' do
     @figure = Figure.find(params[:id])
     @landmark = Landmark.all
+    @title = Title.all
     erb :'figures/edit'
   end
 
@@ -35,9 +38,7 @@ class FiguresController < ApplicationController
     erb :'figures/show'
   end
 
-  # allow the user to edit figure name, and which figure it associatees with 
   patch '/figures/:id' do
-    binding.pry
     @figure = Figure.find(params[:id])
     @figure.update(name: params["figure"]["name"]) 
     
