ec4c532486d6446f4abe89eec7259620982aaa3a
William Pannell <lpann5@gmail.com>
2020-12-01 17:45:02 -0600

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,19 +8,38 @@ class FiguresController < ApplicationController
     erb :'figures/new'
   end
 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
   post '/figures' do
     @figure = Figure.create(params[:figure])
     if !params[:title][:name].empty?
       @figure.titles << Title.create(params[:title])
-    #else
-    #  @figure.title = params[:title]
     end
     if !params[:landmark][:name].empty?
-      @figure.landmarks << Landmark.create(params[:landmarks])
-    #else
-    #  @figure.landmarks = params[:landmarks]
+      @figure.landmarks << Landmark.create(params[:landmark])
     end
     @figure.save
-    redirect to "/figures/#{figure.id}"
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
   end
 end
