491484e85f81f212faf028e58a6405423ab540be
marquesavery <averymarques4@gmail.com>
2020-12-11 21:17:52 -0800

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,10 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  # raise 'Write CreateLandmarks migration here'
+  def change
+    create_table :landmarks do |l|
+      l.string :name
+      l.integer :year_completed
+      l.integer :figure_id 
+    end
+  end
 end
