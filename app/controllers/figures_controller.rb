dcf29f60354eabafefa76eb647913afd552fdc64
JustinBreneman <Justin.Breneman@yahoo.com>
2020-12-23 23:03:36 -0800

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,14 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures/' do
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  get '/figures/:slug' do
+    @figure = Figure.find_by_slug(params(:slug))
+    erb :'figures/show'
+  end
 end
