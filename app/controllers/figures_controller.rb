34b9ef25e1a6a0e199074f444042e6676e4ee464
JustinBreneman <Justin.Breneman@yahoo.com>
2020-12-24 21:37:08 -0800

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,14 +1,34 @@
 class FiguresController < ApplicationController
-  get '/figures/' do
+  get '/figures' do
+    @figures = Figure.all
     erb :'figures/index'
   end
 
   get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
     erb :'figures/new'
   end
 
-  get '/figures/:slug' do
-    @figure = Figure.find_by_slug(params(:slug))
+  post '/figures' do
+    figure = Helper.create_figure(params)
+    redirect to "/figures/#{figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
     erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    figure = Helper.update_figure(params)
+    redirect to "/figures/#{figure.id}"
   end
 end
--- app/lib/slugifiable.rb
+++
@@ -1,9 +0,0 @@
-class Slugifiable
-    def self.call(text)
-        if text.gsub!(/[^0-9A-Za-z ]/, '')
-            text.split(" ").join("-").downcase
-        else
-            text.split(" ").join("-").downcase
-        end
-    end
-end
