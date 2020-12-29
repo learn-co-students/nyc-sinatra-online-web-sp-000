0af960c2f7df48be065e509507d4a58a8cc3a79f
DabooUA <sbardysh@tutanota.com>
2020-12-29 14:52:50 -0600

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,11 +1,55 @@
 class FiguresController < ApplicationController
+  
   get '/figures' do
     @figures = Figure.all 
     erb :'/figures/index'
   end
 
-  get '/figures/id' do
-    @figures = Figure.find_by_id(params[:id])
-    erb :'figures/root'
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @titles = FigureTitle.find_by(figure_id: @figure.id)
+    @landmarks = @figure.landmarks
+    erb :'figures/show'
   end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    @titles = FigureTitle.find_by(figure_id: @figure.id)
+    @landmarks = Landmarks.all
+    erb :'figures/edit'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params['figure'])
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
 end
