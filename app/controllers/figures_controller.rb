677409fb3f4dbe28ec9a7f11ee82af35be851a61
Catherine Gloria <catherinegloria@gmail.com>
2020-12-29 16:51:02 -0800

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,49 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+
+    redirect to "figures/#{@figure.id}"
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params["landmark"])
+    @landmark.year_completed = params["landmark"]["year_completed"]
+    @landmark.save
+
+    #flash[:message] = "Successfully updated landmark."
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,11 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :figure_id
+      t.integer :year_completed
+    end
+  end
 end
+
+#may have to create new landmark file
