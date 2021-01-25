6337a00361292be873a7cb635df69b5d8f039616
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-25 01:20:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < Sinatra::Base
-  set :views, proc { File.join(root, '../views/') }
-  register Sinatra::Twitter::Bootstrap::Assets
-
-  get '/' do
-    erb :"application/index"
-  end
-end
