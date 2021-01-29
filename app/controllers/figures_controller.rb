74dac9b712731ccfa4de050d63b3959295678ccf
concow <ccowan003@comcast.net>
2021-01-28 16:30:01 -0800

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,20 +1,20 @@
 class FiguresController < ApplicationController
   # add controller methods
-  get "/figures" do
+  get '/figures' do
     @figures = Figure.all
     erb :"figures/index"
   end
 
-  get "/figures/new" do
+  get '/figures/new' do
     erb :"figures/new"
   end
 
-  get 'figures/:id' do
+  get '/figures/:id' do      #allows you to see a single Figure
     @figure = Figure.find(params[:id])
     erb :"figures/show"
   end
 
-  get "/figures/:id/edit" do
+  get '/figures/:id/edit' do
     @figure = Figure.find(params[:id])
     erb :"figures/edit"
   end
@@ -23,7 +23,7 @@ class FiguresController < ApplicationController
   #allows create new fig with new titles
   #allows create new fig with new landmark
   #creates checkboxes
-  post "/figures" do
+  post '/figures' do
     @figure = Figure.create(params['figure'])
 
     unless params[:landmark][:name].empty?
@@ -38,7 +38,7 @@ class FiguresController < ApplicationController
     redirect to "/figures/#{@figure.id}"
   end
 
-  patch "/figures/:id" do
+  patch '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
     @figure.update(params[:figure])
 
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,26 +1,26 @@
 class LandmarksController < ApplicationController
   # add controller methods
-  get "/landmarks" do
+  get '/landmarks' do
     @landmarks = Landmark.all
     @figures = Figure.all
     erb :"landmarks/index"
   end
 
-  get "/landmarks/new" do     #view form to create new landmark
+  get '/landmarks/new' do     #view form to create new landmark
     erb :"landmarks/new"
   end
 
-  get "/landmarks/:id" do     #allows you to edit a single landmark
+  get '/landmarks/:id' do     #allows you to edit a single landmark
     @landmark = Landmark.find(params[:id])
     erb :"landmarks/show"
   end
 
-  get "/landmarks/:id/edit" do    #allows to view form to edit sindle landmark?
+  get '/landmarks/:id/edit' do    #allows to view form to edit sindle landmark?
     @landmark = Landmark.find(params[:id])
     erb :"landmarks/edit"
   end
 
-  post "/landmarks/:id" do
+  post '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
 
     @landmark.name = params['landmark']['name']
@@ -30,7 +30,7 @@ class LandmarksController < ApplicationController
     redirect to "/landmarks/#{@landmark.id}"
   end
 
-  post "/landmarks" do            #allows you to create a new landmark
+  post '/landmarks' do            #allows you to create a new landmark
     Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
     redirect "/landmarks"
   end
