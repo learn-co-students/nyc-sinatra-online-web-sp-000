8110c713b15ccb044d2156ea913e54cc063041e3
Alison Williams <apw5011@gmail.com>
2021-02-10 10:59:07 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,56 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do 
+    erb :'/figures/index'
+  end 
+
+  get '/figures/new' do 
+    erb :'/figures/new'
+  end 
+
+  get '/figures/:id' do 
+    @figure = Figure.find_by(params[:id])
+    erb :'figures/show'
+  end 
+
+  get '/figures/:id/edit' do 
+    #binding.pry
+    @figure = Figure.find_by(params[:id])
+    erb :'/figures/edit'
+  end 
+
+  post '/figures' do 
+    #binding.pry
+    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end 
+    
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end 
+
+    @figure.save 
+
+    redirect "/figures/#{@figure.id}"
+  end 
+
+  patch '/figures/:id' do 
+    #binding.pry
+    @figure = Figure.find_by(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end 
+    
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end 
+
+    @figure.save 
+
+    redirect "/figures/#{@figure.id}"
+
+  end 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,32 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks' do 
+    erb :'/landmarks/index'
+  end 
+
+  get '/landmarks/new' do 
+    erb :'landmarks/new'
+  end 
+
+  get '/landmarks/:id' do 
+    @landmark = Landmark.find_by(params[:id])
+    erb :'/landmarks/show'
+  end 
+
+  get '/landmarks/:id/edit' do 
+    @landmark = Landmark.find_by(params[:id])
+    erb :'/landmarks/edit'
+  end 
+
+  post '/landmarks/new' do 
+    @landmark = Landmark.create(params[:landmark])
+    redirect to "/landmarks/#{@landmark.id}"
+  end 
+
+  patch '/landmarks/:id' do 
+    @landmark = Landmark.find_by(params[:id])
+    @landmark.update(params[:landmark])
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end 
 end
