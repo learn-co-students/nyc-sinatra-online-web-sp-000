10342cbf0e21d5bd0bdbe1f129fd8e414f62c75c
github username <github email address>
2021-02-20 15:44:55 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -29,5 +29,30 @@ class FiguresController < ApplicationController
     erb :'/figures/show'
   end
 
+  get "/figures/:id/edit" do 
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
 
+  patch "/figures/:id" do
+    if !params[:figure].keys.include?("title_ids")
+      params[:figure]["title_ids"] = []
+    end
+
+    if !params[:figure].keys.include?("landmark_ids")
+      params[:figure]["landmark_ids"] = []
+    end
+
+    @figure = Figure.find(params[:id])
+    @figure.update(params["figure"])
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
+
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Title.create(name: params["landmark"]["name"])
+    end
+
+    redirect "figures/#{@figure.id}"
+  end
 end
