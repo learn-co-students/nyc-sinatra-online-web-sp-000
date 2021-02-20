e84ce90ebdc74dbe327f4633259220d52eb9bcba
github username <github email address>
2021-02-20 14:06:48 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,28 +10,24 @@ class FiguresController < ApplicationController
   end
 
   post "/figures" do
-    # new figure and the form to edit a figure should allow the user to select from existing landmarks 
-    # and title and create new landmarks and titles to associate to the figure.
-    #binding.pry
-    # @figure = Figure.create(params[:figure])
-        
-    # @figure.title_ids = params[:titles]
+    binding.pry
+     @figure = Figure.create(params[:figure])
 
-    # @figure.landmark_ids = params[:landmarks]
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
 
-    # @figure.save   
-    
-    # # for creating a title from the "text" box, not the checkboxes, we would write something like:
-    # # find a new title through the title[name] params (params[:title][:name]), and add it to the figure instance
-    # # also add the "checked" figure[title_ids][] (params[:figure][:title_ids]) to the figure instance
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    end 
 
-    # redirect to "/figures/#{@figure.id}"
+     redirect to "/figures/#{@figure.id}"
   end
 
-  # get "/figures/:id" do
-  #   @figure = Figure.find(params[:id])
-  #   erb :'/figures/show'
-  # end
+  get "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
 
 
 end
