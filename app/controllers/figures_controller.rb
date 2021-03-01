81aa1e674c7405ec43d39b9c6b625bc1fdc9bb73
Christerle Nounoute <christerlesn@gmail.com>
2021-03-01 01:37:07 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,17 +1,46 @@
 class FiguresController < ApplicationController
-  get '/figures/new' do
-    erb :'figures/new'
+  # add controller methods
+  get '/figures' do  #read - shows all figures
+    @figures = Figure.all
+    erb :'/figures/index'
   end
 
-  post '/figures' do
-    figure = Figure.create(params[:figure])
-    figure.save
-    @figures = Figure.all
-    erb :'figures/index'
+  get '/figures/new' do  #read - shows new figures and titles
+    @titles = Title.all
+    erb :'/figures/new'
   end
 
-  get '/figures' do
-    @figures = Figure.all
-    erb :'figures/index'
+  post '/figures' do  #create - shows new page of landmark
+      @figures = Figure.create(params['figure'])
+    unless params[:landmark][:name].empty?
+      @figures.landmarks << Landmark.create(params[:landmark])
+    end
+    unless params[:title][:name].empty?
+      @figures.titles << Title.create(params[:title])
+    end
+    @figures.save
+    redirect to "/figures/#{@figures.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do  #update - shows page where you actually edit figures by id
+    @figures = Figure.find_by_id(params[:id])
+      @figures.update(params[:figure])
+      unless params[:title][:name].empty?
+        @figures.titles << Title.create(params[:title])
+      end
+      unless params[:landmark][:name].empty?
+        @figures.landmarks << Landmark.create(params[:landmark])
+      end
+      @figures.save
+      redirect to "/figures/#{@figures.id}"
+  end
+  get '/figures/:id' do
+    @figures = Figure.find(params[:id])
+    erb :'figures/show'
   end
 end
