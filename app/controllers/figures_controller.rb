88706688711c158d77362da33f785f8cb4c0549a
DabooUA <sbardysh@tutanota.com>
2021-03-01 21:47:58 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -41,13 +41,14 @@ class FiguresController < ApplicationController
 
   patch '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
-    @figure.update(params[:figure])
+    @figure.update(name: params[:figure][:name])
     unless params[:title][:name].empty?
       @figure.titles << Title.create(params[:title])
     end
     unless params[:landmark][:name].empty?
       @figure.landmarks << Landmark.create(params[:landmark])
     end
+    
     @figure.save
     redirect to "/figures/#{@figure.id}"
   end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,23 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  
+  get '/landmarks' do
+    @landmarks = Landmark.all 
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @figure = Landmark.find_by_id(params[:id])
+    erb :'landmarks/edit'
+  end
+
 end
