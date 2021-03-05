9621ad7e11fab2d6691c6d87d2ca54178a96cf19
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-03-05 16:23:16 -0600

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,37 @@
+require 'pry'
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figures = Figure.all 
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do 
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    binding.pry
+    @figure = Figure.create(params[:figure])
+    @figure.landmark = params["landmarks"]
+    @figure.titles = params["titles"]
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+
+  end
+
+  patch '/figures/:id/edit' do
+
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,22 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all 
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do 
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+
+  end
+
+  get '/landmarks/:id/edit' do
+
+  end
+
+  patch '/landmarks/:id/edit' do
+
+  end
 end
