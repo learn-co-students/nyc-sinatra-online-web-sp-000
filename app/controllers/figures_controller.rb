6f24ca44e9f5abfc06c572bd28549940b5d86990
GoldenPavilion <rcorystine@gmail.com>
2021-04-05 13:40:38 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,28 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures' do
+    @figures = Figure.all
+    
+    erb :"figures/index"
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    
+    erb :"figures/new"
+  end
+
+  post '/figures' do
+    binding.pry
+    #finish creating POST
+    
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    
+    erb :'/figures/show'
+  end
+
 end
