0af48bd9a47c886639fed7a92be390fe888a6b1a
lahb2434 <chrishoughton2434@gmail.com>
2021-04-07 16:06:40 -0400

    Done.

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,3 +1,12 @@
 class Figure < ActiveRecord::Base
-  # add relationships here
+  has_many :landmarks
+  has_many :figure_titles
+  has_many :titles, through: :figure_titles
 end
+
+
+# figure = Figure.create(name: 'Qin Shi Huong')
+# title = Title.find_or_create_by(name: 'Emperor')
+# figure.figure_titles.build(title: title)
+# figure.landmarks.build(name: 'The Great Wall of China', year_completed: 1666)
+# figure.save
