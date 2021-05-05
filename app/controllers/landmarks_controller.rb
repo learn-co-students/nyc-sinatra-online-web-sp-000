a761c5057d02cf73c6579afd179946e3ec6adad2
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-05-05 15:12:27 -0700

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,34 +1,37 @@
 class LandmarksController < ApplicationController
-  # add controller methods
-  get '/landmarks' do 
-    @landmarks= Landmark.all 
+  get "/landmarks" do
+    @landmarks = Landmark.all
     erb :'/landmarks/index'
-  end 
-  
-  get '/landmarks/new' do 
-    erb :'/Landmark/new' 
-  end 
-  
-  post '/landmark' do 
-    
-     @landmark= Landmark.create(name: params[:Landmark][:name], year_completed: params[:Landmark][:year_completed] )
-     @Landmark.save 
-     redirect_to "Landmarks/#{Landmark.id}"
-     
-  end 
-  
-  get '/landmarks/:id/edit' do
-    @Landmark= Landmark.find_by(params[:id])
-    erb "/Landmarks/edit "
-  end 
-  
-   patch '/landmarks/:id' do 
-     @landmark= Landmark.find(params[:id])
-     @landmark.name = 	params[:landmark][:name]
-     @landmark.year_completed = params[:landmark][:year_completed]
-     @landmark.save 
-     redirect_to 'landmarks/#{@landmark.id}'
+  end
 
-   end 
-  
+  get "/landmarks/new" do
+   # @landmark = Landmark.create(name: params[:name], year_completed: params[:year_completed])
+    erb :'/landmarks/new'
+  end
+
+   post "/landmarks" do
+     @landmark = Landmark.create(name: params[:name], year_completed: params[:year_completed])
+     @landmark.save
+     redirect "landmarks/#{landmark.id}"
+   end
+
+   get "/landmarks/:id/edit" do
+     @landmark = Landmark.find(params[:id])
+     erb :'/landmarks/edit'
+   end
+
+   patch "/landmarks/:id" do
+     @landmark = Landmark.find(params[:id])
+     @landmark.name = params[:landmark][:name]
+     @landmark.year_completed = params[:landmark][:year_completed]
+     @landmark.save
+     redirect "landmarks/#{@landmark.id}"
+   end
+
+   get "/landmarks/:id" do
+     @landmark = Landmark.find(params[:id])
+     erb :'/landmarks/show'
+   end
+
 end
+
