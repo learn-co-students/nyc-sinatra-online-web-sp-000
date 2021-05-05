2a75759dca7867618052184458825386e6854b54
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-05-05 13:35:59 -0700

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -9,7 +9,7 @@ class LandmarksController < ApplicationController
     erb :'/Landmark/new' 
   end 
   
-  post '/Landmark' do 
+  post '/landmark' do 
     
      @landmark= Landmark.create(name: params[:Landmark][:name], year_completed: params[:Landmark][:year_completed] )
      @Landmark.save 
@@ -17,11 +17,18 @@ class LandmarksController < ApplicationController
      
   end 
   
-  get '/Landmarks/:id/edit' do
-    @Landmark= Landmark.find(params[:id])
+  get '/landmarks/:id/edit' do
+    @Landmark= Landmark.find_by(params[:id])
     erb "/Landmarks/edit "
   end 
   
-  
+   patch '/landmarks/:id' do 
+     @landmark= Landmark.find(params[:id])
+     @landmark.name = 	params[:landmark][:name]
+     @landmark.year_completed = params[:landmark][:year_completed]
+     @landmark.save 
+     redirect_to 'landmarks/#{@landmark.id}'
+
+   end 
   
 end
