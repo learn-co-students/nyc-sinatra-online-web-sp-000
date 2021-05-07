abf144a9440d84d8415782e6b25d6e76e68220b2
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-05-06 18:42:34 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,6 @@
 class FiguresController < ApplicationController
   # add controller methods
+   
    get '/figures' do 
      @figures = Figure.all 
      erb :"/figures/index"
@@ -10,8 +11,87 @@ class FiguresController < ApplicationController
   end 
    
    post '/figures' do 
+
       @figure =Figure.create( name: params[:figure][:name])
-      
-   end 
+
+      if !params[:figure][:title_ids].nil?
+
+      	  params[:figure][:title_ids].each do |title|
+      		@figure.titles  << Title.find(title)
+      	end 
+
+     end 
+
+      	if !params[:title][:name].strip.empty?
+
+            @title = Title.create(name: params[:title][:name])
+
+      		@figure.titles << @title
+
+      	end 
+
+      	if !params[:figure][:landmark_ids].nil?
+      		params[:figure][:landmark_ids].each do |landmark|
+
+      			@figure.landmarks << Landmark.find(landmark)
+      		end 
+      	end 
+
+      	if !params[:landmark][:name].strip.empty? 
+
+          @landmark= Landmark.create( name: params[:landmark][:name])
+          @figure.landmarks << @landmark
+
+        end
+
+
+         @figure.save 
+
+         redirect_to "/figures/#{@figure.id}"
+       
+       end 
+
+     get '/figures/:id/edit' do 
+
+     	 @figure = Figure.find(params[:id])
+
+     	 erb :'/figures/edit'
+
+     end 
+
+   patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    @figure.name = params[:figure][:name]
+
+    if !params[:figure][:title_ids].nil?
+      params[:figure][:title_ids].each do |title_id|
+        @figure.titles << Title.find(title_id)
+      end
+    end
+
+    if !params[:title][:name].nil?
+      @figure.titles << Title.create(name: params[:title][:name])
+    end
+
+    if !params[:figure][:landmark_ids].nil?
+      params[:figure][:landmark_ids].each do |landmark_id|
+        @figure.landmarks << Landmark.find(landmark_id)
+      end
+    end
+
+    if !params[:landmark][:name].nil?
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+
    
 end
--- db/migrate/20210223215143_create_landmarks.rb
+++ db/migrate/20210223215143_create_landmarks.rb
@@ -1,7 +1,8 @@
 
 class CreateLandmarks < ActiveRecord::Migration
   def change
-      create_table :landmarks do |t|
+    create_table :landmarks do |t|
+
       t.string     :name
       t.integer    :figure_id
       t.integer    :year_completed
