b6a166efac28fb8a04ffa88e2c84425fe3b63a5d
jousethen <jthen93@gmail.com>
2021-05-09 17:38:14 -0400

    Models and Figures New COmplete

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,33 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index' 
+  end
+  
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+
+    erb :'figures/new'
+  end
+  
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
+
+    if !params["landmark"]["name"].empty?
+      landmark = Landmark.create(name: params["landmark"]["name"], year_completed: params["landmark"]["year"], figure_id: @figure.id) 
+      
+    end
+
+    redirect "figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
 end
