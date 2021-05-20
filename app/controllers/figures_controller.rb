b0de9925a5ca0005af8cf2a3ba5451980e12eba5
Michael Dobak <mdobak44@icloud.com>
2021-05-20 15:50:12 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,26 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all.sort_by{|figure| figure.name }
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @figure = Figure.all.sort_by{|figure| figure.name }
+    @titles = Title.all
+    @landmarks = Landmark.all.sort_by{|landmark| landmark.name }
+    erb :'/figures/new'
+  end
+
+  post '/figures/new' do
+    @figure = Figure.create(params[:figure])
+    @figure.landmarks = Landmark.find_or_create_by(name: params[:landmark][:name])
+    @figure.titles = Title.find_or_create_by(name: params[:title][:name])
+    @figure.save
+    redirect to("/figures/#{@figure.id}")
+  end
+
+  get '/figures/id' do
+    
+  end
 end
