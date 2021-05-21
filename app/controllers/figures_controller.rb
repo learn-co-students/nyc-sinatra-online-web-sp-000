6d52e40311c274135533eb24b22f68cb69cec6f5
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-21 17:18:17 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,14 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures/new' do
+    @figure = Figure.new(params)
+    erb :'/figures/new'
+  end
+
+  # post '/figures' do
+  #   @figure = Figure.create(params[:figure])
+  #   @figure.save
+  # end
+
 end
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,3 +1,5 @@
 class Title < ActiveRecord::Base
+  has_many :figure_titles
+  has_many :titles, through: :figure_titles
   # add relationships here
 end
