64e75ef7f735a4aea615f0090c22aeca6949aff2
Chris Baum <Christopher.Baum@valpo.edu>
2021-06-02 21:59:26 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,55 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    #create a new title if applicable
+    if params[:title][:name] != ""
+      @figure.titles << Title.create(params[:title])
+    end
+
+    #create a new landmark if applicable
+    if params[:landmark][:name] != ""
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    @figure = Figure.find_by_id(params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.name = params[:figure][:name]
+
+    if params[:landmark][:name] != ""
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    @figure.save
+
+    redirect("/figures/#{@figure.id}")
+  end
+
+
 end
